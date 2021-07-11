2b7234c9ff73a460da9dc8f7d06fb7c1c867d14a
JoyceQuerubino <joycequerubino5@gmail.com>
2021-07-11 01:25:44 -0300

    Create cards

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -7,22 +7,76 @@ import api from '../services/api';
 
 import { Header } from '../components/Header';
 import { EnviromentButton } from '../components/EnviromentButton';
-
+import { PlantCard } from '../components/PlantCard';
 interface EnviromentProps {
     key: string; 
     title: string;
 }
+interface PlantsProps {
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string],
+    frequency: {
+      times: number;
+      repeat_every: string;
+    }
+}
+
 
 export function PlantSelect(){
 
     const [ enviroments, setEnviroments] = useState<EnviromentProps[]>([]); 
+    const [ plants, setPlants] = useState<PlantsProps[]>([]); 
+    //criar um estado auxiliar (filteredPlants) ao estado 'plants', porque o plants vai trazer todos os dados da API e o estado auxiliar 
+    //vai fazer os filtros, não sendo necessário fazer novamente requisições a cada chamada do estado. 
+    const [ filteredPlants, setFilteredPlants ] = useState<PlantsProps[]>([]); 
+    const [enviormentSelected, setEnviormentSelected] = useState('all');
+
+    
+
+    function handleEnvirormentSelected(category: string){
+        setEnviormentSelected(category);
+
+        //quando clicar no filtro, vamos verificar se a categoria selecionada é 'Todos', 
+        //porque se for, vamos retornar não deixando essa função prosseguir, 
+        // e vamos armazenar no estado das categorias filtradas todas as categorias que haviam sido carregadas pela api. 
+        if(category === 'all'){
+            return setFilteredPlants(plants); 
+        } 
+        //caso contrario, vamos percorrer cada planta verificando se ela possuí a categoria que estamos filtrando.
+        else{
+            const filtered = plants.filter(plant => 
+                plant.environments.includes(category)
+            );
+
+            setFilteredPlants(filtered);
+        }
+    }   
 
     useEffect(() => {
         async function fetchEnvirioment(){
-            const { data } = await api.get('plants_environments'); //que é o nome das categorias que queremos no json
-            setEnviroments(data); 
+            const { data } = await api
+            .get('plants_environments?_sort=title&_order=asc'); //que é o nome das categorias que queremos no json
+            setEnviroments([
+                {
+                    key: 'all', 
+                    title: 'Todos',
+                },
+                ...data
+            ]); 
         }
         fetchEnvirioment();
+    }, [])
+
+    useEffect(() => {
+        async function fetchPlants(){
+            const { data } = await api.get('plants?_sort=name&_order=asc'); //que é o nome das categorias que queremos no json
+            setPlants(data); 
+        }
+        fetchPlants();
     }, [])
 
 
@@ -46,12 +100,26 @@ export function PlantSelect(){
                     renderItem={({item}) => (
                         <EnviromentButton 
                             title={item.title}
+                            //o item.key é igual ao valor que esta no estado 'enviormentSelected'?
+                            active={item.key === enviormentSelected}
+                            onPress={() => handleEnvirormentSelected(item.key)}
                         />
                     )}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     contentContainerStyle={styles.enviromentList}
                 />  
+            </View>
+
+            <View style={styles.plants}>
+                <FlatList 
+                    data={filteredPlants} //passa os dados filtrados
+                    renderItem={( {item} ) => (
+                        <PlantCard data={item} />
+                    )}
+                    showsVerticalScrollIndicator={false} // remover scroll 
+                    numColumns={2} //mostrar a lista em 2 colunas
+                />
             </View>
         </View>
     )
@@ -84,5 +152,10 @@ const styles = StyleSheet.create({
         paddingBottom: 5, 
         marginLeft: 32, 
         marginVertical: 32
-    }
+    }, 
+    plants:{
+        flex: 1, 
+        paddingHorizontal: 32, 
+        justifyContent: 'center'
+    }, 
 })
